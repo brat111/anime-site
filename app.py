26c346c57cf999d0f96371180704debe8945ad42
denisuchiha2007@gmail <denisushiha2007@gmail>
2026-08-22 11:44:44 +0300

    Add sitemap and robots

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -33,6 +33,38 @@ def naruto_episode(episode):
         episode=episode,
         video_id=video_id
     )
+
+
+@app.route("/robots.txt")
+def robots():
+    return Response(
+        """User-agent: *
+Allow: /
+
+Sitemap: https://anime-site-q04r.onrender.com/sitemap.xml
+""",
+        mimetype="text/plain"
+    )
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    urls = [
+        "https://anime-site-q04r.onrender.com/",
+        "https://anime-site-q04r.onrender.com/naruto",
+        "https://anime-site-q04r.onrender.com/naruto/episode/1",
+        "https://anime-site-q04r.onrender.com/naruto/episode/2"
+    ]
+
+    xml = '<?xml version="1.0" encoding="UTF-8"?>'
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+    for url in urls:
+        xml += f"<url><loc>{url}</loc></url>"
+
+    xml += "</urlset>"
+
+    return Response(xml, mimetype="application/xml")
 
 
 if __name__ == "__main__":
